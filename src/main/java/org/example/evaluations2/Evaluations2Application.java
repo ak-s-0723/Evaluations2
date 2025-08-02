9b92e3bf5a6b4e77265e0b9ebf93af891a536b47
khannaanurag1 <khannaanurag1@gmail.com>
2025-08-02 19:12:48 +0530

    initial commit

---
+++ src/main/java/org/example/evaluations2/Evaluations2Application.java
@@ -0,0 +1,13 @@
+package org.example.evaluations2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Evaluations2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Evaluations2Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/evaluations2/Evaluations2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.evaluations2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Evaluations2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
