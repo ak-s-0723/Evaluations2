36461c8f2283cc4d0a98b0e3cd54e4232c7c68f6
khannaanurag1 <khannaanurag1@gmail.com>
2025-08-21 21:27:57 +0530

    Solution

--- src/main/java/org/example/evaluations2/handlers/UrlAuthenticationSuccessHandler.java
+++ src/main/java/org/example/evaluations2/handlers/UrlAuthenticationSuccessHandler.java
@@ -3,12 +3,7 @@ package org.example.evaluations2.handlers;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
-import org.example.evaluations2.models.User;
-import org.example.evaluations2.services.DeviceService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.web.RedirectStrategy;
 import org.springframework.security.web.WebAttributes;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
@@ -17,33 +12,11 @@ import java.io.IOException;
 
 @Component
 public class UrlAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
-
-    @Autowired
-    private DeviceService deviceService;
-
-    @Autowired
-    private RedirectStrategy redirectStrategy;
-    private final String url = "http://localhost:8080/devices/users/user";
-
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
-        loginNotification(authentication, request);
-        redirectStrategy.sendRedirect(request,response,url);
+        response.setContentType("application/json");
+        response.getWriter().write(authentication.getName()+", Welcome !!");
         clearAuthenticationAttributes(request);
-    }
-
-    private void loginNotification(Authentication authentication,
-                                   HttpServletRequest request)  {
-        try {
-            if (authentication.getPrincipal() instanceof UserDetails userDetails) {
-                User user = new User();
-                user.setEmail(userDetails.getUsername());
-                user.setPassword(userDetails.getPassword());
-                deviceService.verifyDevice(user, request);
-            }
-        } catch(Exception e) {
-            throw new RuntimeException(e);
-        }
     }
 
     protected void clearAuthenticationAttributes(HttpServletRequest request) {
